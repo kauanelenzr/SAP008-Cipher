80293aabf0088ac3ef24529ceae2b50f1dbda6e0
Kauane <kauanelenzr@gmail.com>
2022-07-30 15:46:32 -0300

    type error adicionado

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,10 @@
 const cipher = {encode, decode}
 
 function encode(offset, texto) {
+  if (typeof offset != Number && typeof texto != String) {
+    throw TypeError = alert("Digite apenas com letras de A-Z e escolha um número inteiro a partir de 1");
+  }
+
   let resultadoCifra = "";
   for (let i = 0; i < texto.length; i++) {
     let textToCode = ((texto.charCodeAt(i) -65 +offset) %26) +65;
@@ -11,6 +15,10 @@ function encode(offset, texto) {
 }
 
 function decode(offset, texto) {
+  if (typeof offset != Number && typeof texto != String) {
+    throw TypeError = alert("Digite apenas com letras de A-Z e escolha um número inteiro a partir de 1");
+  }
+  
   let resultadoDecifra = "";
   for (let i = 0; i < texto.length; i++) {
     let textToCode = ((texto.charCodeAt(i) -90 -offset) %26) +90;
